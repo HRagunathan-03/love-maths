9bc43063d71466807e2fbd7bd6b10f48562a70e2
HRagunathan-03 <hragunathan@googlemail.com>
2025-09-16 18:01:22 +0100

    add function to calculate answer

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,11 @@
 //Wait for the DOM to finish loading before running the game.
 //Get the button elements and add event listeners to them.
 
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function () {
     let buttons = document.getElementsByTagName("button");
 
     for (let button of buttons) {
-        button.addEventListener("click", function() {
+        button.addEventListener("click", function () {
             if (this.getAttribute("data-type") === "submit") {
                 alert("You clicked submit!");
             } else {
@@ -22,16 +22,16 @@ document.addEventListener("DOMContentLoaded", function() {
  * The main game "loop" that is called when the script is first loaded, and after the user's answer.
  */
 function runGame(gameType) {
-//Creates two randome numbers between 1 and 25.
-let num1 = Math.floor(Math.random() * 25) +1;
-let num2 = Math.floor(Math.random() * 25) +1;
+    //Creates two randome numbers between 1 and 25.
+    let num1 = Math.floor(Math.random() * 25) + 1;
+    let num2 = Math.floor(Math.random() * 25) + 1;
 
-if (gameType === "addition") {
-    displayAdditionQuestion(num1, num2);
-} else {
-    alert(`Unknown game type: ${gameType}`);
-    throw `Uknown game type: ${gameType}. Aborting!`;
-}
+    if (gameType === "addition") {
+        displayAdditionQuestion(num1, num2);
+    } else {
+        alert(`Unknown game type: ${gameType}`);
+        throw `Uknown game type: ${gameType}. Aborting!`;
+    }
 
 
 }
@@ -41,9 +41,20 @@ if (gameType === "addition") {
 function checkAnswer() {
 
 }
-
+/**
+ * Gets the operands (the numbers) and the operator (plus, minus etc.) directly from the DOM, and returns the correct answer.
+ */
 function calculateCorrectAnswer() {
+let operand1 = parseInt(document.getElementById("operand1").innerText);
+let operand2 = parseInt(document.getElementById("operand2").innerText);
+let operator = document.getElementById("operator").innerText;
 
+if (operator === "+") {
+    return [operand1 + operand2, "addition"];
+} else {
+    alert(`Unimplemented operator ${operator}`);
+    throw `Unimplemented operator ${operator}. Aborting!`;
+}
 }
 
 function incrementScore() {
@@ -63,13 +74,13 @@ function displayAdditionQuestion(operand1, operand2) {
 }
 
 function displaySubtractQuestion() {
-    
+
 }
 
 function displayMultiplyQuestion() {
-    
+
 }
 
 function displayDivideQuestion() {
-    
+
 }
